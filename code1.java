9423bb1c51895f3aaf3fd0113c351020f74bd16a
kitty011488 <generose.mangaring@citycollegeoftagaytay.edu.ph>
2025-04-18 15:34:55 +0800

    java project

---
+++ code1.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+public class code1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            System.out.print("enter a letter: ");
+        String letter = scan.nextLine().toUpperCase();;
+
+        if (letter.equals("A")){ 
+        System.out.print("this is a vowels");
+        }
+
+        else if (letter.equals("E")){ 
+            System.out.print("this is a vowels");
+            }
+
+        else if (letter.equals("I")){ 
+                System.out.print("this is a vowels");
+                }
+
+        else if (letter.equals("O")){ 
+                    System.out.print("this is a vowels");
+                    }
+
+        else if (letter.equals("U")){ 
+                        System.out.print("this is a vowels");
+                        }
+        else{
+            System.out.print("its a consonant");
+        }
+
+        
+        /* 
+        System.out.println("1. addition ");
+        System.out.println("2. subtraction");
+        System.out.println("3. multiplication");
+        System.out.println("4. division");
+        System.out.println("5. exponent");
+        System.out.println("6. modulos");
+        System.out.print("enter your choice: ");
+        int choice = scan.nextInt();
+
+
+        System.out.print("\nenter a number: ");
+        int num1 = scan.nextInt();
+        System.out.print("enter a number: ");
+        int num2 = scan.nextInt();
+
+        if (choice == 1){
+            System.out.print(num1 + num2);
+        }
+        else if (choice == 2 ){
+            System.out.print(num1 - num2);
+        }
+        else if (choice == 3 ){
+            System.out.print(num1 *  num2);
+        }
+        else if (choice == 4 ){
+            System.out.print(num1 /  num2);
+        }
+        else if (choice == 5 ){
+            System.out.print(num1 ^ num2);
+        }
+        else if (choice == 6 ){
+            System.out.print(num1 %  num2);
+        }*/
+        
+        
+
+
+    }
+}
+
+        
+
+        
